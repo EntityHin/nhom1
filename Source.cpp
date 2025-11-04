b73bbe62465f4cae88532481d35adc7a35b49964
Chan Hung <hinphu7@gmail.com>
2025-11-04 09:03:36 +0700

    nt

--- Source.cpp
+++ Source.cpp
@@ -4,8 +4,20 @@ using namespace std;
 int tong(int a, int b){
 	return a + b;
 }
-int hieu(int a, int b);
-int tich(int a, int b);
+int hieu(int a, int b){
+	return a-b;
+}
+int tich(int a, int b){
+	return a*b;
+}
+
+bool nguyento(int n){
+	if(n<2) return false;
+	for(int i=2; i*i<=n;i++)
+		if(n%i==0)
+			return false;
+		return true;
+}
 float thuong(int a, int b);
 int main()
 {
@@ -16,6 +28,16 @@ int main()
 	cout << "Nhap b: ";
 	cin>> b;
 	cout<<"Tong = " << tong(a,b) << endl;
+	cout<<"Hieu = " << hieu(a,b) << endl;
+	cout<<"Tich = "<<tich(a,b)<<endl;
+	
+	int n;
+	cout << "Nhap n de kt nguyen to: ";
+	cin >> n;
+	if(nguyento(n))
+		cout << n << " la so nguyen to";
+	else
+		cout << n << " khong phai so nguyen to";
 	system("pause");
 	return 0;
 }
