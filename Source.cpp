d0c1da52f88538eb936f2d963131f0db8506c9dc
khang <huynhkhang2672004@gmail.com>
2025-11-04 09:13:30 +0700

    thuong

--- Source.cpp
+++ Source.cpp
@@ -5,13 +5,24 @@ int tong(int a, int b){
 	return a + b;
 }
 int hieu(int a, int b){
-	return a - b;
+	return a-b;
 }
-int tich(int a, int b);
-float thuong(int a, int b);
-void name(){
-	cout << "Khang";
+int tich(int a, int b){
+	return a*b;
 }
+
+bool nguyento(int n){
+	if(n<2) return false;
+	for(int i=2; i*i<=n;i++)
+		if(n%i==0)
+			return false;
+		return true;
+}
+float thuong(int a, int b)
+{
+	return a/b;
+}
+
 int main()
 {
 	int a,b;
@@ -22,6 +33,16 @@ int main()
 	cin>> b;
 	cout<<"Tong = " << tong(a,b) << endl;
 	cout<<"Hieu = " << hieu(a,b) << endl;
+	cout<<"Tich = "<<tich(a,b)<<endl;
+	cout<<"Thuong = "<<tich(a,b)<<endl;
+	
+	int n;
+	cout << "Nhap n de kt nguyen to: ";
+	cin >> n;
+	if(nguyento(n))
+		cout << n << " la so nguyen to";
+	else
+		cout << n << " khong phai so nguyen to";
 	system("pause");
 	return 0;
 }
