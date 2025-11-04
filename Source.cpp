b45ca0657d39f62d0d9d5d7deb76793fba31c468
Bao <baokun6b@gmail.com>
2025-11-04 08:42:09 +0700

    C

--- Source.cpp
+++ Source.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
 
 using namespace std;
-int tong(int a, int b);
-int hieu(int a, int b);
+int tong(int a, int b){
+	return a + b;
+}
+int hieu(int a, int b){
+	return a-b;
+}
 int tich(int a, int b){
 	return a*b;
-};
+}
 float thuong(int a, int b);
 int main()
 {
 	int a,b;
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-    cout << "Nhap a: ";
+	cout << "Nhap a: ";
 	cin >>a;
 	cout << "Nhap b: ";
 	cin>> b;
-	cout<<"Tong = " << tich(a,b) << endl;
+	cout<<"Tong = " << tong(a,b) << endl;
+	cout<<"Hieu = " << hieu(a,b) << endl;
+	cout<<"Tich = "<<tich(a,b)<<endl;
 	system("pause");
 	return 0;
 }
